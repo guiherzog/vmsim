94c5c08804110b48812d4f5ff9aed97ea258d408
Lucas Carneiro <lucas.rod.carneiro@gmail.com>
2017-06-25 03:43:38 -0300

    - Showing MMU execution on index.html;
     - Using play to iterate through the instructions list.

--- simulator.js
+++ simulator.js
@@ -1,14 +1,8 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-var virtualmemory = require('./simulator/virtualmemory');
-
-// console.log(virtualmemory);
-
-
 const numberPages = 8;
 const numberProcesses = 16;
-document.getElementById("numberProcesses").innerHTML = numberProcesses;
 
 var processList = [];
 for (var i = 0; i < numberProcesses; i++) {
@@ -40,7 +34,6 @@ var secondaryMemoryList = [];
 var instructionList = [];
 // Random number of instructions between 20 and 5
 const numberInstructions = Math.floor(Math.random() * (21 - 5)) + 5;
-console.log(numberInstructions + " Instructions");
 // Generating random instructions
 for (var i = 0; i < numberInstructions; i++) {
 	instructionList.push({
@@ -49,31 +42,31 @@ for (var i = 0; i < numberInstructions; i++) {
 	});
 }
 
-// Time to run some instructions
-for (var i = 0; i < instructionList.length; i++) {
-	var pageLocation = checkMemory(0, instructionList[i]);
-	if (pageLocation.memoryType != 0){
-		// push() returns the new length of the array
-		var frame = primaryMemoryList.push(instructionList[i]) - 1;
-		processList[instructionList[i].processId][instructionList[i].pageId].p = true;
-		processList[instructionList[i].processId][instructionList[i].pageId].frame = frame;
-		switch (pageLocation.memoryType){
-			case 1: // Virtual
-				virtualMemoryList.slice(pageLocation.index, 1);
-				break;
-			case 2: // Swap
-				swapMemoryList.slice(pageLocation.index, 1);
-				break;
+initRender();
+runInstruction();
+
+function runInstruction(){
+	if (instructionList.length > 0) {
+		console.log("Running an instruction...");
+		var pageLocation = checkMemory(0, instructionList[0]);
+		if (pageLocation.memoryType != 0){
+			// push() returns the new length of the array
+			var frame = primaryMemoryList.push(instructionList[0]) - 1;
+			processList[instructionList[0].processId][instructionList[0].pageId].p = true;
+			processList[instructionList[0].processId][instructionList[0].pageId].frame = frame;
+			switch (pageLocation.memoryType){
+				case 1: // Virtual
+					virtualMemoryList.splice(pageLocation.index, 1);
+					break;
+				case 2: // Swap
+					swapMemoryList.splice(pageLocation.index, 1);
+					break;
+			}
 		}
+		instructionList.splice(0, 1);
+		renderData();
 	}
 }
-
-console.log("Primary Memory:");
-console.dir(primaryMemoryList);
-console.log("Virtual Memory:");
-console.dir(virtualMemoryList);
-console.log("Swap Memory:");
-console.dir(swapMemoryList);
 
 function checkMemory(memoryType, page, pageToSave=null){
 	var memoryList;
@@ -130,5 +123,64 @@ function checkMemory(memoryType, page, pageToSave=null){
 			}
 			return checkMemory(++memoryType, page);
 		}
+	}
+}
+
+function initRender(){
+	document.getElementById("numberProcesses").innerHTML = numberProcesses;
+	document.getElementById("playButton").addEventListener("click", function(){
+		runInstruction();
+	});
+}
+
+function renderData(){
+	document.getElementById("instruction").innerHTML = `Executando tempo <strong>${numberInstructions - instructionList.length} de ${numberInstructions}</strong>`;
+
+	document.getElementById("primaryMemorySize").innerHTML = primaryMemoryList.length * pageSize + "/" + primaryMemorySize * pageSize + " <small>Bytes alocados.</small>";
+	document.getElementById("virtualMemorySize").innerHTML = virtualMemoryList.length * pageSize + "/" + virtualMemorySize * pageSize + " <small>Bytes alocados.</small>";
+	document.getElementById("swapMemorySize").innerHTML = swapMemoryList.length * pageSize + "/" + swapMemorySize * pageSize + " <small>Bytes alocados.</small>";
+
+	if (primaryMemoryList.length > 0)
+		document.getElementById("lastPagePrimaryMemory").innerHTML = `Última Página Adicionada: <strong>Página ${primaryMemoryList[primaryMemoryList.length-1].pageId} do Processo ${primaryMemoryList[primaryMemoryList.length-1].processId}</strong>`;
+	else
+		document.getElementById("lastPagePrimaryMemory").innerHTML = `Última Página Adicionada: <strong>--</strong>`;
+	if (virtualMemoryList.length > 0)
+		document.getElementById("lastPageVirtualMemory").innerHTML = `Última Página Adicionada: <strong>Página ${virtualMemoryList[virtualMemoryList.length-1].pageId} do Processo ${virtualMemoryList[virtualMemoryList.length-1].processId}</strong>`;
+	else
+		document.getElementById("lastPageVirtualMemory").innerHTML = `Última Página Adicionada: <strong>--</strong>`;
+	if (swapMemoryList.length > 0)
+		document.getElementById("lastPageSwapMemory").innerHTML = `Última Página Adicionada: <strong>Página ${swapMemoryList[swapMemoryList.length-1].pageId} do Processo ${swapMemoryList[swapMemoryList.length-1].processId}</strong>`;
+	else
+		document.getElementById("lastPageSwapMemory").innerHTML = `Última Página Adicionada: <strong>--</strong>`;
+
+	document.getElementById("primaryMemoryList").innerHTML = "";
+	for (var i = 0; i < primaryMemoryList.length; i++) {
+		document.getElementById("primaryMemoryList").innerHTML += `
+			<tr>
+				<td>${i}</td>
+				<td>${primaryMemoryList[i].pageId}</td>
+				<td>${primaryMemoryList[i].processId}</td>
+			</tr>
+		`;
+	}
+	document.getElementById("virtualMemoryList").innerHTML = "";
+	for (var i = 0; i < virtualMemoryList.length; i++) {
+		document.getElementById("virtualMemoryList").innerHTML += `
+			<tr>
+				<td>${i}</td>
+				<td>${virtualMemoryList[i].pageId}</td>
+				<td>${virtualMemoryList[i].processId}</td>
+			</tr>
+		`;
+	}
+	document.getElementById("swapMemoryList").innerHTML = "";
+	for (var i = 0; i < swapMemoryList.length; i++) {
+		document.getElementById("swapMemoryList").innerHTML += `
+			<tr>
+				<td>${i}</td>
+				<td>${swapMemoryList[i].pageId}</td>
+				<td>${swapMemoryList[i].processId}</td>
+			</tr>
+		`;
 	}
 }
--- simulator/virtualmemory.js
+++
@@ -1,3 +0,0 @@
-// This file contains all structures, data and methods used by the virtual memory.
-const data = [200, 20, 22, 123, 123, 123];
-module.exports = data;
